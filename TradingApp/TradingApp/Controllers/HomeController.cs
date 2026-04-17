6707ebb2e98e2dc44cd2fc16cd94dcafdde90459
Ryan Gorman <ryang201299@gmail.com>
2026-04-17 11:12:38 +0100

    Converted to backend API only with Swagger

--- TradingApp/TradingApp/Controllers/HomeController.cs
+++ TradingApp/TradingApp/Controllers/HomeController.cs
@@ -6,20 +6,5 @@ namespace TradingApp.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
